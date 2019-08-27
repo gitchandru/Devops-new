18b087270dc259f1eb9d290239fbd23c2541a433
gitchandru <ask4chandra@gmail.com>
2019-08-27 16:05:26 +0530

    Addded for Generating War file
    Addded for Generating War file

--- src/main/java/com/chandru/demo/simpledemo/SimpledemoApplication.java
+++ src/main/java/com/chandru/demo/simpledemo/SimpledemoApplication.java
@@ -2,12 +2,37 @@ package com.chandru.demo.simpledemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class SimpledemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpledemoApplication.class, args);
-	}
+/*
+ * @SpringBootApplication public class SimpledemoApplication {
+ * 
+ * public static void main(String[] args) {
+ * SpringApplication.run(SimpledemoApplication.class, args); }
+ * 
+ * }
+ */
+ 
+ 
 
-}
+
+
+  // For Generating War file instead of .jar file uncomment below code and  comment above code
+  
+  
+  
+  @SpringBootApplication public class SimpledemoApplication extends
+  SpringBootServletInitializer {
+  
+  @Override protected SpringApplicationBuilder
+  configure(SpringApplicationBuilder application) { return
+  application.sources(SimpledemoApplication.class); }
+  
+  public static void main(String[] args) {
+  SpringApplication.run(SimpledemoApplication.class, args); }
+  
+  }
+ 
+ 
